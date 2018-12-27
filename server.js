f73bf0b99f014afbc2f7cb1c5228bd60964fcec3
FrixOficial <40075146+FrixOficial@users.noreply.github.com>
2018-12-27 17:45:14 -0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
-const manager = new Discord.ShardingManager('./bot.js', {token: config.token});
-manager.spawn().then(() => {
-    console.log(`El bot ha sido encendido! Actualmente ejecuntando ${manager.totalShards} shards.`);
-
-}).catch((err) => {
-    console.log(err);
-});
